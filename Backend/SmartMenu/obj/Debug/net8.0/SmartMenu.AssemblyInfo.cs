62afaa8a7fbc6f4066c9c3017f9309770ee95f08
BanBannBannn <tranvangiaban@gmail.com>
2024-05-31 11:31:01 +0700

    pull

--- Backend/SmartMenu/obj/Debug/net8.0/SmartMenu.AssemblyInfo.cs
+++ Backend/SmartMenu/obj/Debug/net8.0/SmartMenu.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartMenu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+706dc5b602b9da29a12db39d2763a59b372046b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d68190aa67eab96c2d6400b8ad82e58d5eb7eea1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartMenu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartMenu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
