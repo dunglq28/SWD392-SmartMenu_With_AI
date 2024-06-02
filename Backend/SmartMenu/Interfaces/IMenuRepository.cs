87d8c1cf68c024deba4ccf2917396ed485661364
TamBuiInHood <144221275+TamBuiInHood@users.noreply.github.com>
2024-06-02 21:51:13 +0700

    ADD FUTURE - CRUD Menu

---
+++ Backend/SmartMenu/Interfaces/IMenuRepository.cs
@@ -0,0 +1,15 @@
+﻿using SmartMenu.DTOs;
+using SmartMenu.Entities;
+using SmartMenu.Payloads.Requests;
+
+namespace SmartMenu.Interfaces
+{
+    public interface IMenuRepository : IGenericRepository<Menu,MenuDTo>
+    {
+        Task<MenuDTo> AddAsync(AddMenuRequest reqObj);
+
+        Task<MenuDTo?> UpdateAsync(int id, bool isActive);
+        Task<IEnumerable<MenuDTo>?> GetAllByBrandIDAsync( int brandID);
+
+    }
+}
---
+++ Backend/SmartMenu/Validations/AddMenuValidation.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using SmartMenu.Payloads.Requests;
+
+namespace SmartMenu.Validations
+{
+    public class AddMenuValidation : AbstractValidator<AddMenuRequest>
+    {
+        public AddMenuValidation()
+        {
+            RuleFor(x => x.IsActive)
+                .NotNull()
+                . WithMessage("Trạng thái chưa chính xác");
+               
+            RuleFor(x => x.BrandId)
+                .NotNull()
+                .WithMessage("Brand ID chưa chính xác");
+
+        }
+    }
+}
