c43235afa4cb087ba95bc61b29983e8be5c57a88
BanBannBannn <tranvangiaban@gmail.com>
2024-07-11 17:25:08 +0700

    add api add list ProductList

--- Backend/FSU.SmartMenuWithAI.Service/ISerivice/IProductListService.cs
+++ Backend/FSU.SmartMenuWithAI.Service/ISerivice/IProductListService.cs
@@ -1,5 +1,6 @@
 ﻿using FSU.SmartMenuWithAI.Repository.Entities;
 using FSU.SmartMenuWithAI.Service.Models;
+using FSU.SmartMenuWithAI.Service.Models.ListProduct;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,7 @@ namespace FSU.SmartMenuWithAI.Service.ISerivice
     {
         Task<ProductListDTO> GetByID(int productId, int listId);
         Task<ProductListDTO> Insert(int productId, int listId, int indexInList, int brandId);
+        Task<List<ProductListDTO>> Insert2(int brandId, List<ListProductDetail> listProductDetails);
         Task<ProductListDTO> UpdateAsync(int productId, int listId, int index, int newProductId);
         Task<bool> DeleteAsync(int productId, int listId);
 
