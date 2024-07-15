90abc798961daf893ca93ef764f89160f4684083
DNG <119122847+dunglq28@users.noreply.github.com>
2024-07-15 12:03:56 +0700

    Update MenuController.cs

--- Backend/FSU.SmartMenuWithAI.API/Controllers/MenuController.cs
+++ Backend/FSU.SmartMenuWithAI.API/Controllers/MenuController.cs
@@ -255,7 +255,7 @@ namespace FSU.SmartMenuWithAI.API.Controllers
             }
         }
         [Authorize(Roles = UserRoles.BrandManager + "," +UserRoles.Store)]
-        [HttpGet(APIRoutes.Menu.RecommendMenu, Name = "recommend-menu-async")]
+        [HttpPost(APIRoutes.Menu.RecommendMenu, Name = "recommend-menu-async")]
         public async Task<IActionResult> RecommendMenuAsync([FromForm] RecomentMenuRequest reqobj)
         {
             try
